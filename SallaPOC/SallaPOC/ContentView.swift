b3a948546c0ee11d47d58685e69ba1fdb7fd066f
Mohammad-KS90 <mohmmad90suliman@gmail.com>
2024-09-04 16:47:47 +0300

    first commit

---
+++ SallaPOC/SallaPOC/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SallaPOC
+//
+//  Created by Mohammad kh Suliman on 04/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+// un-commit this code to use render to view the page in your XCode
+//#Preview {
+//    ContentView()
+//}
---
+++ SallaPOC/SallaPOC/SallaPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SallaPOCApp.swift
+//  SallaPOC
+//
+//  Created by Mohammad kh Suliman on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SallaPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
